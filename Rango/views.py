39e19df0708b2c0b072e819e85c6c4a3c1fb446a
Karim Noble <nkarim496@gmail.com>
2017-05-14 14:13:55 +0300

    template for index view, handled static and media files settings in urls.py and settings.py

--- Rango/views.py
+++ Rango/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Rango says hello mudak!')
+    context_dic = {'boldmessage': "Rango says: When concatenating system paths together,"
+                                  " always use os.path.join()."}
+    return render(request, 'rango/index.html', context=context_dic)
+
+def about(request):
+    return HttpResponse('Rango says it is about page. | <a href="/rango">Rango</a> |')
