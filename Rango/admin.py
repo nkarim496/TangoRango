76c42da315a27783f22780eed8a0a7ded9960ad2
Karim Noble <nkarim496@gmail.com>
2017-05-22 09:53:15 +0300

    1. models.py - added in slug to Category 2. admin.py - added class CategoryAdmin 3. urls.py - mapped show_category 4. views.py - added show_category(), edited index()

--- Rango/admin.py
+++ Rango/admin.py
@@ -3,9 +3,13 @@ from Rango.models import Category, Page
 
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
+    list_display = ('title', 'category', 'url', 'views')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
 
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- Rango/views.py
+++ Rango/views.py
@@ -1,10 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from Rango.models import Category, Page
 
 def index(request):
-    context_dic = {'boldmessage': "Rango says: When concatenating system paths together,"
-                                  " always use os.path.join()."}
-    return render(request, 'rango/index.html', context=context_dic)
+    categories_list = Category.objects.order_by('-likes')[:5]
+    context = {'categories': categories_list}
+
+    # top five most viewed pages
+    top_five_pages = Page.objects.order_by('-views')[:5]
+    context["top_five_pages"] = top_five_pages
+    return render(request, 'rango/index.html', context=context)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context['category'] = category
+        context['pages'] = pages
+    except Category.DoesNotExist:
+        context['category'] = None
+        context['pages'] = None
+    return render(request, 'rango/category.html', context)
